bc9d53e1c0527e79f590d9a1b16478d221ddfe87
mohdomi <omairm.s.chouhan@gmail.com>
2025-06-17 13:00:11 +0000

    backend build package.json

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,13 +9,13 @@ const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-console.log(process.env.frontend_URL);
+console.log(process.env.FRONTEND_URL);
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 const server = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: process.env.Frontend_URL,
+        origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST']
     }
 });
