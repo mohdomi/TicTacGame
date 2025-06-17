faaf3e8aa19598c18e80c9030bdc1c83feb69ddc
mohdomi <omairm.s.chouhan@gmail.com>
2025-06-17 14:17:38 +0000

    cors error fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(cors());
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: `${process.env.FRONTEND_URL}/*`,
+        origin: process.env.FRONTEND_URL?.replace(/\/$/, ''), 
         methods: ['GET', 'POST']
     }
 });
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
@@ -6,8 +6,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <StrictMode>
+ 
       <App />
-    </StrictMode>
+   
   </BrowserRouter>
 )
